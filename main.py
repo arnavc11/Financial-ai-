9efc3954129607f23ff21f6983f6d66d5c97e955
arnavc11 <arnav110211@gmail.com>
2026-05-07 11:04:46 +0530

    Add Uvicorn server run command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import uvicorn
+uvicorn.run("main:app", host="0.0.0.0", port=port)
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
