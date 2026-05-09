2feb880bf4ab3d5e0204e3ee3fe65bd4671aa73f
arnavc11 <arnav110211@gmail.com>
2026-05-09 12:15:12 +0530

    Add COINGECKO_BASE_URL to Config class

--- config/settings.py
+++ config/settings.py
@@ -4,9 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    COINGECKO_BASE_URL: str = "https://api.coingecko.com/api/v3
+    
     GROQ_API_KEY: str = os.getenv("GROQ_API_KEY", "")
     GROQ_MODEL: str = "llama-3.3-70b-versatile"  
-
+    
     GNEWS_API_KEY: str = os.getenv("GNEWS_API_KEY", "")
     GNEWS_BASE_URL: str = "https://gnews.io/api/v4"
     
